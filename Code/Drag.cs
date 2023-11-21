94e224e2d046c26da6fc1dc0a76618e4f820b188
sljur <116686483+sljur@users.noreply.github.com>
2023-11-21 10:39:24 -0700

    Create Drag.cs
    Added in the drag system for our weapons objects. As of right now, we can pick objects up, but they get lost in the layers of the game.

---
+++ Code/Drag.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    private bool dragging = false;
+    private Vector3 offset;
+
+    void Update()
+    {
+        if (dragging)
+        {
+            //Move object, taking into account original offset.
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    
+    }
+
+    private void OnMouseDown()
+    {
+        //Record the difference between the objects centre, and the clicked point on the camera plane
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        dragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+
+        //stop dragging
+        dragging = false;
+    }
+}
