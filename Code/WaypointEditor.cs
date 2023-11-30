7d4a73e0e6ae6490d3954e97fb603c230efb0a6b
sljur <116686483+sljur@users.noreply.github.com>
2023-11-30 14:01:52 -0700

    Update WaypointEditor.cs
    Added additional commentary about the code

--- Code/WaypointEditor.cs
+++ Code/WaypointEditor.cs
@@ -11,16 +11,17 @@ public class WaypointEditor : Editor
 
     private void OnSceneGUI()
     {
-
+        //This right here determines way point color when setting 
         Handles.color = Color.cyan;
         for (int i = 0; i < Waypoint.Points.Length; i++)
         {
             EditorGUI.BeginChangeCheck();
 
-            //cretae handles
+            //creates handles of the waypoints
             Vector3 currentWaypointPoint = Waypoint.CurrentPosition + Waypoint.Points[i];
             var fmh_22_86_638344600082203399 = Quaternion.identity; Vector3 newWaypointPoint = Handles.FreeMoveHandle( currentWaypointPoint, 0.7f, new Vector3(0.3f,0.3f,0.3f), Handles.SphereHandleCap);
-            //create text
+           
+            //create text for the waypoint; this also helps with how the waypoint looks
             GUIStyle textStyle = new GUIStyle();
             textStyle.fontStyle = FontStyle.Bold;
             textStyle.fontSize = 16;
@@ -28,7 +29,8 @@ public class WaypointEditor : Editor
             Vector3 textAlligment = Vector3.down * 0.35f + Vector3.right * 0.35f;
             Handles.Label(Waypoint.CurrentPosition + Waypoint.Points[i] + textAlligment, $"{i + 1}", textStyle);
             EditorGUI.EndChangeCheck();
-
+            
+            //Helps remove or add new waypoint in the Unity interface
             if (EditorGUI.EndChangeCheck())
             {
               Undo.RecordObject(target, "FreeMoveHandle");
