3a30885bba1dc164c29829cd7faf50a8c6ef755b
meaghan66 <102330088+meaghan66@users.noreply.github.com>
2023-11-30 18:30:01 -0700

    Update Shroom.cs
    Fixed up comments and added some code for hearts

--- Code/Shroom.cs
+++ Code/Shroom.cs
@@ -2,40 +2,65 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Shroom : MonoBehaviour
 {
-    private float MoveSpeed = 2.0f;
+    // declare variables
+    public float MoveSpeed = 2.0f;
     private SpriteRenderer _spriteRenderer;
     private Vector3 _lastPointPosition;
     private int _currentWaypointIndex;
     private EnemyHealth _enemyHealth;
+    public EnemyHealth EnemyHealth;
+    private Vector3 CurrentPointPosition;
 
     public Waypoint Waypoint;
     public Action<Shroom> OnEndReached;
 
-    private bool isMovementStopped;
+    public bool isMovementStopped;
+    public Image heart1;
+    public Image heart2;
+    public Image heart3;
+    private Weapon _enemies;
 
     void Start()
     {
+        // get the components for enemy health, enemies, and renderer
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _enemyHealth = GetComponent<EnemyHealth>();
+        EnemyHealth = GetComponent<EnemyHealth>();
+        _enemies = GetComponent<Weapon>();
+
+        // set the number of hearts based on the level
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        if(currentSceneName == "_Scene2")
+        {
+            Destroy(heart1);
+        }
+        else if (currentSceneName == "_Scene3")
+        {
+            Destroy(heart2);
+        }
     }
 
     void Update()
     {
-        if (!isMovementStopped)
-        {
-            Move();
-            Rotate();
+        // move the shroom on the waypoints
+        Move();
+        CurrentPointPosition = Waypoint.GetWayPointPosition(_currentWaypointIndex);
+        Rotate();
 
-            if (CurrentPointPositionReached())
-            {
-                UpdateCurrentPointIndex();
-            }
+        if (CurrentPointPositionReached())
+        {
+            UpdateCurrentPointIndex();
         }
+
+        _lastPointPosition = Waypoint.GetWayPointPosition(_currentWaypointIndex); ;
     }
 
+    // move the shrooms along the waypoint
     private void Move()
     {
         Vector3 targetPosition = Waypoint.GetWayPointPosition(_currentWaypointIndex);
@@ -43,11 +68,10 @@ public class Shroom : MonoBehaviour
             MoveSpeed * Time.deltaTime);
     }
 
+    // rotate the Shroom based on the waypoints
     private void Rotate()
     {
-        Vector3 currentPosition = transform.position;
-
-        if (Waypoint.Points[_currentWaypointIndex].x > currentPosition.x)
+        if (CurrentPointPosition.x > _lastPointPosition.x)
         {
             _spriteRenderer.flipX = false;
         }
@@ -55,11 +79,10 @@ public class Shroom : MonoBehaviour
         {
             _spriteRenderer.flipX = true;
         }
-
-        _lastPointPosition = currentPosition;
     }
 
 
+    // check if the shrooms have gotten to the next waypoint
     private bool CurrentPointPositionReached()
     {
         float distanceToNextPointPosition = (transform.position - Waypoint.GetWayPointPosition(_currentWaypointIndex)).magnitude;
@@ -71,6 +94,7 @@ public class Shroom : MonoBehaviour
         return false;
     }
 
+    // get the current waypoint
     private void UpdateCurrentPointIndex()
     {
         int lastWaypointIndex = Waypoint.Points.Length - 1;
@@ -84,20 +108,41 @@ public class Shroom : MonoBehaviour
         }
     }
 
+    // if the shroom reaches the end point
     private void EndPointReached()
     {
         OnEndReached?.Invoke(this);
-        _enemyHealth.ResetHealth();
+        EnemyHealth.ResetHealth();
         ObjectPooler.ReturnToPool(gameObject);
+
+        // destroy a heart based on how many hearts are left and how many shrooms have crossed
+        if (heart1 != null)
+        {
+            Destroy(heart1);
+        }
+        else if (heart2 != null)
+        {
+            Destroy(heart2);
+        }
+        else if (heart3 != null)
+        {
+            Destroy(heart3);
+            SceneManager.LoadScene("GameOverScene");
+        }
+
+
     }
 
+    // set the movement to stop and set to true
     public void StopMovement()
     {
         isMovementStopped = true;
     }
 
+    // resume the movement and set to false
     public void ResumeMovement()
     {
         isMovementStopped = false;
     }
+
 }
