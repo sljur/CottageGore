35a88fc6d0860ded48db11542f002f9df51f64cb
meaghan66 <102330088+meaghan66@users.noreply.github.com>
2023-11-30 18:28:12 -0700

    Update EnemyHealth.cs
    Fixed up comments

--- Code/EnemyHealth.cs
+++ Code/EnemyHealth.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
+    // declare variables
     public static Action<Shroom> OnEnemyKilled;
     public static Action<Shroom> OnEnemyHit;
 
@@ -21,24 +22,21 @@ public class EnemyHealth : MonoBehaviour
 
     void Start()
     {
+        // set the health bar and get the Shroom component for the shrooms
         CreateHealthBar();
         CurrentHealth = initialHealth;
         _enemy = GetComponent<Shroom>();
     }
 
+    // set the amount in the health bar with the current health
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            DealDamage(5f);
-        }
-
-
-        _healthBar.fillAmount = Mathf.Lerp(_healthBar.fillAmount, 
+        _healthBar.fillAmount = Mathf.Lerp(_healthBar.fillAmount,
             CurrentHealth / maxHealth, Time.deltaTime * 10f);
 
     }
 
+    // create the health bar and the fill amount
     private void CreateHealthBar()
     {
         GameObject newBar = Instantiate(healthBarPrefab, barPosition.position, Quaternion.identity);
@@ -47,6 +45,7 @@ public class EnemyHealth : MonoBehaviour
         _healthBar = container.FillAmountImage;
     }
 
+    // deal damage to the shrooms and kill if the health is less than or equal to 0
     public void DealDamage(float damageReceived)
     {
         CurrentHealth -= damageReceived;
@@ -61,12 +60,14 @@ public class EnemyHealth : MonoBehaviour
         }
     }
 
+    // kill the enemy and set the enemy to false
     public void Die()
     {
         OnEnemyKilled?.Invoke(_enemy);
         gameObject.SetActive(false);
     }
 
+    // set the health back to the initial health
     public void ResetHealth()
     {
         CurrentHealth = initialHealth;
