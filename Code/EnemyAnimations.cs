883d794028bb98b875e2becfdfd4119900a86fdb
meaghan66 <102330088+meaghan66@users.noreply.github.com>
2023-11-30 18:26:57 -0700

    Update EnemyAnimations.cs

--- Code/EnemyAnimations.cs
+++ Code/EnemyAnimations.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class EnemyAnimations : MonoBehaviour
 {
-    public GameObject deathParticles;
+    // declare variables
     private Animator _animator;
     private Shroom _enemy;
     private EnemyHealth _enemyHealth;
 
     void Start()
     {
+        // get the components for animator, enemy, and enemyhealth
         _animator = GetComponent<Animator>();
         _enemy = GetComponent<Shroom>();
         _enemyHealth = GetComponent<EnemyHealth>();
     }
 
+    // play the hit animation when the weapon hits the enemy
     private void PlayHurtAnimation()
     {
         _animator.SetTrigger("Hit");
     }
 
+    // get the length of the animation
     private float GetCurrentAnimationLength()
     {
         float animationLength = _animator.GetCurrentAnimatorStateInfo(0).length;
         return animationLength;
     }
 
+    // full range of motion for the hurt animation
     private IEnumerator PlayHurt()
     {
         _enemy.StopMovement();
@@ -37,38 +41,42 @@ public class EnemyAnimations : MonoBehaviour
 
     }
 
+    // full range of motion for the dead animation
     private IEnumerator PlayDead()
     {
         _enemy.StopMovement();
-        Instantiate(deathParticles, transform.position, Quaternion.identity);
         yield return new WaitForSeconds(0.1f);
         _enemy.ResumeMovement();
         _enemyHealth.ResetHealth();
         ObjectPooler.ReturnToPool(_enemy.gameObject);
     }
 
+    // play the hit animation when finding the enemy
     private void EnemyHit(Shroom enemy)
     {
-        if (_enemy = enemy)
+        if (_enemy == enemy)
         {
             StartCoroutine(PlayHurt());
         }
     }
 
+    // play the dead animation when finding the enemy
     private void EnemyDead(Shroom enemy)
     {
-        if (_enemy = enemy)
+        if (_enemy == enemy)
         {
             StartCoroutine(PlayDead());
         }
     }
-
+    
+    // on enable for enemy hit and enemy killed
     private void OnEnable()
     {
         EnemyHealth.OnEnemyHit += EnemyHit;
         EnemyHealth.OnEnemyKilled += EnemyDead;
     }
 
+    // on disable for enemy hit and enemy killed
     private void OnDisable()
     {
         EnemyHealth.OnEnemyHit -= EnemyHit;
